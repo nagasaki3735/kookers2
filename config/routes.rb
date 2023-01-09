26d034a02ab1ff24a6c759911c3a24df5acf9ba2
EC2 Default User <ec2-user@ip-172-31-34-129.ap-northeast-1.compute.internal>
2023-01-09 14:57:18 +0000

    [Add]4-25 ヘッダーのエラー手直し中

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :edit]
-  resources :books, only: [:new, :index, :show]
-  devise_for :users
-  root to: 'homes#top'
-  resources :users, only: [:show, :edit, :update]
+devise_for :users
+root to: 'homes#top'
+resources :books, only: [:new, :index, :show]
+ 
   end
+  
